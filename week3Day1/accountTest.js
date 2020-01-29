02ca381a921bfe8e18f6006d6a2feaa1fa066d05
gbfasika <46835650+gbfasika@users.noreply.github.com>
2020-01-28 18:28:35 -0600

    Account

---
+++ week3Day1/accountTest.js
@@ -0,0 +1,26 @@
+(function (){
+    "use strict";
+
+    const assert = chai.assert;
+    let acc = new Account(123);
+
+    describe('Testing With Mocha and Chai.', function(){
+        it('Assertion passed : The balance of just created account is 0.', function(){
+            assert.equal(acc.getBalance(),0);
+        });
+
+        it('Assertion passed: The account number matched.', function(){
+            assert.equal(acc.getNumber(),123);
+        });
+
+        it('Assertion passed: Account balance updated after deposit.', function(){
+            acc.deposit(100)
+            assert.equal(acc.getBalance(),100);
+        });
+
+        it('Assertion passed : Account balance updated after withdrawal.', function(){
+            acc.withdraw(50);
+            assert.equal(acc.getBalance(),50);
+        });
+    });
+})();
