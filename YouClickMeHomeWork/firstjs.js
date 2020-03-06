bbfd3e196b457d3f9e8352d6bd0c3ba9c2d7113f
gbfasika <46835650+gbfasika@users.noreply.github.com>
2020-03-06 17:36:45 -0600

    TipCalculatorUpdate

---
+++ YouClickMeHomeWork/firstjs.js
@@ -0,0 +1,19 @@
+var x =100;
+console.log(typeof x);
+let callBack = [];
+for(var i =0; i <=5; i++){
+    callBack[i] = function(){
+        return i * 2;
+    }
+}
+alert(i);
+callBack[0] = 2;
+callBack[1] =200;
+const Pi = 3.141569;
+console.log(Pi);
+console.log("3"+ 2);
+console.log("5"==5.0);
+console.log("5"===5.0);
+const name = "Sara Yifru Hagos";
+console.log(name.substring(0, name.indexOf(" ")));
+
